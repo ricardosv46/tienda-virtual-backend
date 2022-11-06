0ce3f0ec759ed1a6f2df06bba13120f8709c8f7b
RICARDO SOLIS <ricardosv46@gmail.com>
2022-11-06 14:56:20 -0500

    update

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,11 @@ import { graphqlUploadExpress } from 'graphql-upload'
 import { ApolloCtx } from './interface'
 import * as dotenv from 'dotenv'
 dotenv.config()
-import { ApolloServerPluginLandingPageLocalDefault, ApolloServerPluginLandingPageProductionDefault } from 'apollo-server-core'
+import {
+  ApolloServerPluginLandingPageLocalDefault,
+  ApolloServerPluginLandingPageProductionDefault,
+  ApolloServerPluginInlineTraceDisabled
+} from 'apollo-server-core'
 
 const PORT = process.env.PORT
 
@@ -16,16 +20,14 @@ const main = async () => {
     await connectDB()
     const apolloServer = new ApolloServer({
       context: (ctx: ApolloCtx) => ctx,
-      schema: await buildSchema({ resolvers, validate: false })
-      // plugins: [
-      //   // Install a landing page plugin based on NODE_ENV
-      //   process.env.NODE_ENV === 'production'
-      //     ? ApolloServerPluginLandingPageProductionDefault({
-      //         graphRef: 'my-graph-id@my-graph-variant',
-      //         footer: false
-      //       })
-      //     : ApolloServerPluginLandingPageLocalDefault({ footer: false })
-      // ]
+      schema: await buildSchema({ resolvers, validate: false }),
+      plugins: [
+        // Install a landing page plugin based on NODE_ENV
+        process.env.NODE_ENV === 'production'
+          ? ApolloServerPluginLandingPageProductionDefault()
+          : ApolloServerPluginLandingPageLocalDefault({ embed: true }),
+        ApolloServerPluginInlineTraceDisabled()
+      ]
     })
     app.use(graphqlUploadExpress())
     await apolloServer.start()
